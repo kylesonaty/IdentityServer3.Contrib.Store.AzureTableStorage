7dbecfca959af46e26a76a47e92f8307280a9a53
Kyle Sonaty <kyle.sonaty@gmail.com>
2016-02-25 15:19:08 -0500

    Add client store claim converter.

--- src/IdentityServer3.Contrib.Store.AzureTableStorage/AzureTableStorageClientStore.cs
+++ src/IdentityServer3.Contrib.Store.AzureTableStorage/AzureTableStorageClientStore.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using IdentityServer3.Contrib.Store.AzureTableStorage.Serialization;
 using IdentityServer3.Core.Models;
 using IdentityServer3.Core.Services;
 using Microsoft.WindowsAzure.Storage;
@@ -10,6 +12,11 @@ namespace IdentityServer3.Contrib.Store.AzureTableStorage
     public class AzureTableStorageClientStore: IClientStore
     {
         private readonly CloudTable _table;
+
+        private readonly JsonSerializerSettings _settings = new JsonSerializerSettings
+        {
+            Converters = new List<JsonConverter> {new ClaimConverter()}
+        };
 
         public AzureTableStorageClientStore(string connectionString, string tableName = "Clients")
         {
@@ -24,7 +31,7 @@ namespace IdentityServer3.Contrib.Store.AzureTableStorage
             var op = TableOperation.Retrieve<ClientEntity>(clientId.GetParitionKey(), clientId);
             var result = await _table.ExecuteAsync(op);
             var client = result.Result as ClientEntity;
-            return client != null ? JsonConvert.DeserializeObject<Client>(client.Json) : null;
+            return client != null ? JsonConvert.DeserializeObject<Client>(client.Json, _settings) : null;
         }
     }
 }
