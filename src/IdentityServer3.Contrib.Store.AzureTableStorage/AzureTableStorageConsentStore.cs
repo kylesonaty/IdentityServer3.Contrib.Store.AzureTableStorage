02426920eb904857b3edb2b308c5a87f18365514
Kyle Sonaty <kyle.sonaty@gmail.com>
2016-02-28 20:36:08 -0500

    Add consent store.

---
+++ src/IdentityServer3.Contrib.Store.AzureTableStorage/AzureTableStorageConsentStore.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace IdentityServer3.Contrib.Store.AzureTableStorage
+{
+    public class AzureTableStorageConsentStore : IConsentStore
+    {
+        private readonly CloudTable _table;
+
+        /// <summary>
+        /// Creates a new instance of the Azure Table Storage consent store
+        /// </summary>
+        /// <param name="connectionString">Table storage connection string</param>
+        /// <param name="tableName">Optional table name.</param>
+        public AzureTableStorageConsentStore(string connectionString, string tableName = "Consent")
+        {
+            var account = CloudStorageAccount.Parse(connectionString);
+            var client = account.CreateCloudTableClient();
+            _table = client.GetTableReference(tableName);
+            _table.CreateIfNotExists();
+        }
+
+        /// <summary>
+        /// Retrieves all the consent for a subject
+        /// </summary>
+        /// <param name="subject">The subject</param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Consent>> LoadAllAsync(string subject)
+        {
+            var query = new TableQuery<ConsentEntity>().Where(
+                TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, subject));
+            var list = new List<ConsentEntity>();
+            TableContinuationToken continuationToken = null;
+            do
+            {
+                var result = await _table.ExecuteQuerySegmentedAsync(query, continuationToken);
+                continuationToken = result.ContinuationToken;
+                list.AddRange(result.Results);
+            } while (continuationToken != null);
+            return list.Select(c => new Consent { ClientId = c.RowKey, Subject = c.PartitionKey, Scopes = c.Scopes.Split(',')});
+        }
+
+        /// <summary>
+        /// Removes the consent for a subject and client
+        /// </summary>
+        /// <param name="subject">The subject</param>
+        /// <param name="client">The client</param>
+        /// <returns></returns>
+        public async Task RevokeAsync(string subject, string client)
+        {
+            var entity = new ConsentEntity
+            {
+                PartitionKey = subject,
+                RowKey = client,
+                ETag = "*"
+            };
+            var op = TableOperation.Delete(entity);
+            await _table.ExecuteAsync(op);
+        }
+
+        /// <summary>
+        /// Loads the subject's prior consent for the client
+        /// </summary>
+        /// <param name="subject">The subject</param>
+        /// <param name="client">The client</param>
+        /// <returns></returns>
+        public async Task<Consent> LoadAsync(string subject, string client)
+        {
+            var op = TableOperation.Retrieve<ConsentEntity>(subject, client);
+            var result = await _table.ExecuteAsync(op);
+            var entity = result.Result as ConsentEntity;
+            if (entity != null)
+            {
+                return new Consent
+                {
+                    ClientId = entity.RowKey,
+                    Subject = entity.PartitionKey,
+                    Scopes = entity.Scopes.Split(',')
+                };
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Updates the consent for a subject and client.
+        /// </summary>
+        /// <param name="consent">The consent</param>
+        /// <returns></returns>
+        public async Task UpdateAsync(Consent consent)
+        {
+            var entity = new ConsentEntity
+            {
+                PartitionKey = consent.Subject,
+                RowKey = consent.ClientId,
+                Scopes = string.Join(",",consent.Scopes)
+            };
+            var op = TableOperation.InsertOrReplace(entity);
+            await _table.ExecuteAsync(op);
+        }
+    }
+}
---
+++ src/IdentityServer3.Contrib.Store.AzureTableStorage/ConsentEntity.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace IdentityServer3.Contrib.Store.AzureTableStorage
+{
+    internal class ConsentEntity : TableEntity
+    {
+        public string Scopes { get; set; }
+    }
+}
